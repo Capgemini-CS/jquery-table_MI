9e8ea0886e2703a0b06b348c4342fe85afefe395
IMARGHIO <ioan.marghioala@capgemini.com>
2022-04-19 13:01:00 +0300

    call gorest api

--- index.js
+++ index.js
@@ -25,3 +25,66 @@ $(".table tbody").on('click', '.delete-button', function () {
 })
 
 //Edit button
+
+
+
+$(document).ready(function () {
+
+    getTutorials();
+
+
+    function getTutorials() {
+        $('#tutorialsBody').html('');
+        $.ajax({
+            url: 'https://gorest.co.in/public/v2/users',
+            method: 'get',
+            dataType: 'json',
+            data: {
+                test: 'test data'
+            },
+            success: function (data) {
+                $(data).each(function (i, user) {
+                    $('#tutorialsBody').append($("<tr>")
+                        .append($("<td>").append(user.id))
+                        .append($("<td>").append(user.name))
+                        .append($("<td>").append(user.email))
+                        .append($("<td>").append(user.gender))
+                        .append($("<td>").append(user.status)));
+                });
+                //loadButtons();
+            }
+        });
+    }
+
+    function getOneTutorial(id) {
+        $.ajax({
+            url: 'http://localhost:3000/api/tutorials/' + id,
+            method: 'get',
+            dataType: 'json',
+            success: function (data) {
+                $($("#updateForm")[0].tutId).val(data._id);
+                $($("#updateForm")[0].updateNum).val(data.tutorialNumber);
+                $($("#updateForm")[0].updateTitle).val(data.title);
+                $($("#updateForm")[0].updateAuthor).val(data.author);
+                $($("#updateForm")[0].updateType).val(data.type);
+                $("#updateForm").show();
+            }
+        });
+    }
+
+    function loadButtons() {
+        $(".editTut").click(function (e) {
+            getOneTutorial($($(this)[0]).data("tutid"));
+            e.preventDefault();
+        });
+
+        $(".deleteTut").click(function (e) {
+            deleteTutorial($($(this)[0]).data("tutid"));
+            e.preventDefault();
+        })
+    }
+
+
+
+});
+
