9da4b736a0e9f9aa03ccc87b6b3467d4205907a9
IMARGHIO <ioan.marghioala@capgemini.com>
2022-04-19 12:41:31 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-$(document).ready(function(){
-    $("#btnPostPerson").click(function(){
+//Post button
+$(document).ready(function () {
+    $("#btnPostPerson").click(function () {
         var firstname = $("#firstname").val();
         var lastname = $("#lastname").val();
         var age = $("#age").val();
-        $(".table tbody tr").last().after(
-            '<tr>'+
-                '<td>'+firstname+'</td>'+
-                '<td>'+lastname+'</td>'+
-                '<td>'+age+'</td>'+
+        $(".table tbody").append(
+            '<tr>' +
+            '<td>' + firstname + '</td>' +
+            '<td>' + lastname + '</td>' +
+            '<td>' + age + '</td>' +
             '</tr>'
+        );
+        $('.table tbody > tr:last-child').append(
+            '<td> <i class="ui-icon ui-icon-pencil edit-button"></i> </td>' +
+            '<td> <i class="ui-icon ui-icon-trash delete-button"></i> </td>'
         );
     })
 })
+
+
+//Delete button
+$(".table tbody").on('click', '.delete-button', function () {
+    $(this).closest('tr').remove();
+})
+
+//Edit button
